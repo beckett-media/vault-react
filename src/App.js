e297c69402719d7ce8eac6482ddfddbfdb8c87b9
Dan <dbaumfeld@beckett.com>
2022-06-16 09:05:27 -0700

    feat: add react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Homepage from './app/components/Homepage/Homepage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <Homepage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
 }
 
 export default App;
