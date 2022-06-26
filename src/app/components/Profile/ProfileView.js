03357eb6c60244086d8e476a7c8e79bf5dc0d300
Dan <dbaumfeld@beckett.com>
2022-06-26 15:18:43 -0700

    fix gallery page layout

---
+++ src/app/components/Profile/ProfileView.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { getUser } from '../../services/user';
+import './profile.scss';
+
+const ProfileView = () => {
+  // todo: set is loading
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    getUser().then((data) => setUser(data));
+  }, []);
+
+  return (
+    <Container fluid>
+      <div className="row">
+        <div className="col" align="right">
+          {user.name} <br />
+          {user.email}
+        </div>
+        <div className="col">
+          <img className="img-thumbnail profile-image-thumb" src={user.img} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default ProfileView;
