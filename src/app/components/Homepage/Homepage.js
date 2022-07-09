b17e7ce33ac13aeae14c8b924b5a07111157db7b
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-09 02:52:18 +0200

    built and styled homepage

--- src/app/components/Homepage/Homepage.js
+++ src/app/components/Homepage/Homepage.js
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Collapse, Container, Row } from 'react-bootstrap';
+import { Col, Collapse, Container, Row, Carousel } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 // import { resetForm } from '../../state/actions';
 // import { initialState } from '../../state/store/rootReducer';
 import { validEmail, validPhone } from '../Validation/regex';
+
 import InterestForm from './InterestForm';
+import Card from './Card';
 import './Homepage.scss';
+
+const cardData = [
+  {
+    title: 'Top Ten Cards',
+    body: 'Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+    cta: 'Discover More',
+  },
+  {
+    title: 'New Releases',
+    body: 'Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+    cta: 'Shop Now',
+  },
+  {
+    title: 'New Releases',
+    body: 'Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+    cta: 'Shop Now',
+  },
+];
 
 const Homepage = () => {
   const [open, setOpen] = useState(false);
@@ -54,28 +74,89 @@ const Homepage = () => {
     // finish, before firing off the post request
   };
   return (
-    <Container>
-      <Row className='justify-content-md-center mt-2'>
-        <Collapse in={open} timeout={3000}>
-          <Col className='title' align='center'>
-            {'Pioneer the Frontier of Digital & Physical Collectibles'}
-          </Col>
-        </Collapse>
-      </Row>
-      <Row className='justify-content-md-center mt-1 mb-3'>
-        <Col className='title' align='center'>
-          <input
-            type='button'
-            className='rounded-pill ghost-btn btn-sm'
-            value='Begin your Journey'
-          />
-        </Col>
-      </Row>
-      <InterestForm
-        formSubmission={formSubmission}
-        formSubmitted={formSubmitted}
-      />
-    </Container>
+    <div className='page-wrapper'>
+      <section className='section_home-carousel'>
+        <div className='page-padding'>
+          <div className='container-large'>
+            <div className='home-carousel_component'>
+              <div className='home-carousel_layout'>
+                <div className='home-carousel_carousel-wrapper'>
+                  <Carousel
+                    className='w-100 h-100'
+                    controls={false}
+                    nextIcon={
+                      <span
+                        aria-hidden='true'
+                        className='carousel-control-next-icon'
+                      />
+                    }
+                  >
+                    <Carousel.Item>
+                      <img src='' alt='' />
+                      <Carousel.Caption>This is a test</Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img src='' alt='' />
+                      <Carousel.Caption>This is a test</Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img src='' alt='' />
+                      <Carousel.Caption>This is a test</Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
+                </div>
+                <h1 className='home-carousel_heading'>Beckett Vault</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='section_home-content'>
+        <div className='page-padding'>
+          <div className='container-large'>
+            <div fluid className='home-content_layout'>
+              <Row className='home-content_content-wrapper'>
+                <div className='home-content_heading'>Vault topics</div>
+                <div className='home-content_body'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore
+                </div>
+              </Row>
+              <Row className='home-content_cards-wrapper gy-4'>
+                {cardData.map((data, index) => (
+                  <Col key={index} sm={12} md={4}>
+                    <Card title={data.title} body={data.body} cta={data.cta} />
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    // <Container>
+    //   <Row className='justify-content-md-center mt-2'>
+    //     <Collapse in={open} timeout={3000}>
+    //       <Col className='title' align='center'>
+    //         {'Pioneer the Frontier of Digital & Physical Collectibles'}
+    //       </Col>
+    //     </Collapse>
+    //   </Row>
+    //   <Row className='justify-content-md-center mt-1 mb-3'>
+    //     <Col className='title' align='center'>
+    //       <input
+    //         type='button'
+    //         className='rounded-pill ghost-btn btn-sm'
+    //         value='Begin your Journey'
+    //       />
+    //     </Col>
+    //   </Row>
+    //   <InterestForm
+    //     formSubmission={formSubmission}
+    //     formSubmitted={formSubmitted}
+    //   />
+    // </Container>
   );
 };
 
