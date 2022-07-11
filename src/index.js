caeeaabc5be0f2ee98d315ed4070f0cccfbf6be3
Dan <dbaumfeld@beckett.com>
2022-07-10 19:40:04 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './app/state/store/store.js';
 import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
@@ -10,10 +8,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
   </React.StrictMode>,
 );
