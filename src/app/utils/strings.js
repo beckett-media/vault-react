228f3632a05ee9cc8e9b873a5544bbd6c3772dfe
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-13 17:20:32 +0200

    created price formatter util

--- src/app/utils/strings.js
+++ src/app/utils/strings.js
@@ -1,3 +1,8 @@
 export const trimString = (str, length) => {
   return str.length > length ? `${str.slice(0, length + 1)}...` : str;
 };
+
+export const formatPrice = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
