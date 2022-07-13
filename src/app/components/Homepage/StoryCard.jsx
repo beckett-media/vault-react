c49d0ab19cb7dd15d2dc2a4c96af00753e8cad78
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-13 17:21:17 +0200

    updated component name for homepage cards

--- src/app/components/Homepage/StoryCard.jsx
+++ src/app/components/Homepage/StoryCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-import './Card.scss';
+import './StoryCard.scss';
 
 const Card = ({ title, body, cta }, props) => {
   return (
