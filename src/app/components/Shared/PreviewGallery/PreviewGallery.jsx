497e3a229f23182ae4384eeea4f0d42184306597
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-14 02:06:38 +0200

    created preview gallery component

---
+++ src/app/components/Shared/PreviewGallery/PreviewGallery.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ItemCard from '../ItemCard/ItemCard';
+
+import './PreviewGallery.scss';
+
+const PreviewGallery = ({ data, title, link }, props) => {
+  return (
+    <div className='preview-gallery_component'>
+      <div className='preview-gallery_layout'>
+        <div className='preview-gallery_text-wrapper'>
+          <div className='preview-gallery_heading'>{title}</div>
+          <div className='preview-gallery_link-wrapper'>
+            <Link to={`/${link}`}>See more</Link>
+          </div>
+        </div>
+        <div className='preview-gallery_gallery-wrapper'>
+          {data.slice(0, 4).map((item, index) => (
+            <ItemCard item={item} key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreviewGallery;
