68e1868be231c98466d4220cedfd4d4334e7b0c7
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-15 01:39:58 +0200

    added scrollToTop functionality for all routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './app/components/Shared/ScrollToTop/ScrollToTop';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -9,6 +10,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
