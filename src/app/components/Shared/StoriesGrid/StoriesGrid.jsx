cbbd5a75c94ad155c8822bc418a92cef8df53109
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-14 02:06:31 +0200

    created stories grid component

---
+++ src/app/components/Shared/StoriesGrid/StoriesGrid.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import StoryCard from '../StoryCard/StoryCard';
+
+import './StoriesGrid.scss';
+
+const StoriesGrid = ({ data }, props) => {
+  return (
+    <Row className='stories-grid_component gy-4'>
+      {data.map((story, index) => (
+        <Col key={index} sm={12} md={4}>
+          <StoryCard title={story.title} body={story.body} cta={story.cta} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default StoriesGrid;
