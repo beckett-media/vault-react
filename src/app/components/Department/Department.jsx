84f124f99929dfe8b87269c26f7785f62f2c6617
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-16 00:47:46 +0200

    Continued implmenting deparment page

--- src/app/components/Department/Department.jsx
+++ src/app/components/Department/Department.jsx
@@ -2,14 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import DisplayImage from '../Shared/DisplayImage/DisplayImage';
+import ItemGallery from '../Shared/ItemGallery/ItemGallery';
 
+import './Departments.scss';
 import hero from '../../assets/vault-market-hero.png';
+import { getMarketItems } from '../../services/items';
 
 const Department = () => {
   const { param } = useParams();
   const [category, setCategory] = useState(param);
+  const [marketItems, setMarketItems] = useState([]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    getMarketItems().then((data) => setMarketItems(data));
+  }, []);
 
   return (
     <div className='page-wrapper'>
@@ -20,11 +26,18 @@ const Department = () => {
           </div>
         </div>
       </div>
-      <div className='section_department-main-gallery'></div>
+      <div className='section_department-main-gallery'>
+        <div className='page-padding'>
+          <div className='container-large'>
+            <ItemGallery data={marketItems} />
+          </div>
+        </div>
+      </div>
       <div className='section_department-extended-gallery'>
         <div className='page-padding'>
           <div className='container-large'>
             <DisplayImage size={'medium'} image={hero} />
+            <ItemGallery data={marketItems} isInfinite={true} />
           </div>
         </div>
       </div>
