959c2e1345770c528cf4040cca079b3e94fc9c33
sDoddBeckett <SDodd@beckett.com>
2022-07-18 11:06:36 -0500

    refactoring to primary history component

--- src/app/pages/History/SubmissionHistory.js
+++ src/app/pages/History/SubmissionHistory.js
@@ -29,17 +29,7 @@ const SubmissionHistory = () => {
   }, []);
 
   return (
-    <Container className='py-2 sub-box'>
-      <h2 className='fs-3 pb-3'>Submission History</h2>
-      <Row>
-        <Col xs={8}>
-          <h3 className='fs-4'>Title</h3>
-        </Col>
-        <Col xs={3}>
-          <h3 className='fs-4'>Date Created</h3>
-        </Col>
-        <Col xs={1} />
-      </Row>
+    <>
       {submissions.map((sub) => {
         return (
           <div key={sub.submission_id}>
@@ -70,7 +60,7 @@ const SubmissionHistory = () => {
           </div>
         );
       })}
-    </Container>
+    </>
   );
 };
 
