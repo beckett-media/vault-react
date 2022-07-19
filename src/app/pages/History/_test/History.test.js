abebcbb329c57181728d82a75b09a9284f6b6534
Dan <dbaumfeld@beckett.com>
2022-07-19 09:09:30 -0700

    chore: remove history test

--- src/app/pages/History/_test/History.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SubmissionHistory from '../SubmissionHistory';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const root = ReactDOM.createRoot(div);
-  root.render(<SubmissionHistory />);
-});
