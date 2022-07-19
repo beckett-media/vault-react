d658e7cd8570e522c5712e3352564ab83f97826d
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-19 21:39:02 +0200

    built out profile form

--- src/app/pages/Profile/Profile.js
+++ src/app/pages/Profile/Profile.js
@@ -9,17 +9,36 @@ const Profile = () => {
   const authContext = useContext(AuthContext);
   const user = mapCognitoToUser(authContext.attrInfo);
   const [userState, setUserState] = useState(user);
+  const [isShippingSame, setIsShippingSame] = useState(false);
 
   const updateUserState = (tempItem) => setUserState({ ...userState, ...tempItem });
 
-  const submitUpdateUser = async () => {
+  const syncSubmissionAddresses = () => {
+    const syncedAddresses = {
+      shipCountry: userState.country,
+      shipAddressLine1: userState.addressLine1,
+      shipAddressLine2: userState.addressLine2,
+      shipCity: userState.city,
+      shipState: userState.state,
+      shipZipcode: userState.zipcode,
+    };
+
+    updateUserState(syncedAddresses);
+  };
+
+  const submitUpdateUser = async (submission) => {
+    submission.preventDefault();
+    if (isShippingSame) {
+      syncSubmissionAddresses();
+    }
+
     // TODO: on error?
     await authContext.setAttributes(mapUserToCognito(userState));
   };
 
   return (
     <Container>
-      <Form>
+      <Form noValidate onSubmit={submitUpdateUser}>
         <Row className='justify-content-center m-2'>
           <Col lg='6'>
             <Row className='mb-2'>
@@ -75,6 +94,7 @@ const Profile = () => {
                         type='text'
                         value={userState.addressLine1}
                         onChange={(e) => updateUserState({ addressLine1: e.target.value })}
+                        className='mb-2'
                       />
                       <Form.Control
                         type='text'
@@ -83,7 +103,7 @@ const Profile = () => {
                       />
                     </Form.Group>
                   </Row>
-                  <Row className='mb-2'>
+                  <Row>
                     <Col>
                       <Form.Group>
                         <Form.Label>City</Form.Label>
@@ -109,15 +129,98 @@ const Profile = () => {
                         <Form.Label>Zip</Form.Label>
                         <Form.Control
                           type='text'
-                          value={userState.zip}
-                          onChange={(e) => updateUserState({ zip: e.target.value })}
+                          value={userState.zipcode}
+                          onChange={(e) => updateUserState({ zipcode: e.target.value })}
                         />
                       </Form.Group>
                     </Col>
                   </Row>
+                  <Row>
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control
+                          type='text'
+                          value={userState.phone}
+                          onChange={(e) => updateUserState({ phone: e.target.value })}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row className='my-4'>
+                    <Col>
+                      <Form.Check
+                        onClick={() => setIsShippingSame(!isShippingSame)}
+                        type='checkbox'
+                        label='Check if shipping address is the same'
+                      />
+                    </Col>
+                  </Row>
+                  {!isShippingSame && (
+                    <>
+                      <Row className='mb-2'>
+                        <Form.Group>
+                          <Form.Label>Shipping Country/Region</Form.Label>
+                          <Form.Control
+                            type='text'
+                            value={userState.shipCountry}
+                            onChange={(e) => updateUserState({ shipCountry: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Row>
+                      <Row className='mb-2'>
+                        <Form.Group>
+                          <Form.Label>Shipping Address</Form.Label>
+                          <Form.Control
+                            type='text'
+                            value={userState.shipAddressLine1}
+                            onChange={(e) => updateUserState({ shipAddressLine1: e.target.value })}
+                            className='mb-2'
+                          />
+                          <Form.Control
+                            type='text'
+                            value={userState.shipAddressLine2}
+                            onChange={(e) => updateUserState({ shipAddressLine2: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Row>
+                      <Row className='mb-2'>
+                        <Col>
+                          <Form.Group>
+                            <Form.Label>Shipping City</Form.Label>
+                            <Form.Control
+                              type='text'
+                              value={userState.shipCity}
+                              onChange={(e) => updateUserState({ shipCity: e.target.value })}
+                            />
+                          </Form.Group>
+                        </Col>
+                        <Col>
+                          <Form.Group>
+                            <Form.Label>Shipping State</Form.Label>
+                            <Form.Control
+                              type='text'
+                              value={userState.shipState}
+                              onChange={(e) => updateUserState({ shipState: e.target.value })}
+                            />
+                          </Form.Group>
+                        </Col>
+                        <Col>
+                          <Form.Group>
+                            <Form.Label>Shipping Zip</Form.Label>
+                            <Form.Control
+                              type='text'
+                              value={userState.shipZipcode}
+                              onChange={(e) => updateUserState({ shipZipcode: e.target.value })}
+                            />
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                    </>
+                  )}
                   <Row className='mb-2'>
                     <Col>
-                      <Button onClick={() => submitUpdateUser()}>Save</Button>
+                      <Button type='submit'>Save</Button>
                     </Col>
                   </Row>
                 </Card.Body>
