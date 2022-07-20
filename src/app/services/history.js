0a856aeed2e25b3ca6111e36663923515008031f
sDoddBeckett <SDodd@beckett.com>
2022-07-20 11:25:26 -0500

    removed console log

--- src/app/services/history.js
+++ src/app/services/history.js
@@ -7,7 +7,6 @@ export const getHistory = async (userName) => {
   return userName && axios
     .get(`${config.BASE_URL}/marketplace/action/user/${userName}`)
     .then((res) => {
-      console.log('ran and got: ', userName, res)
       return res;
     }).catch(err => console.log('ran and got: ', userName, err));
 };
