c988a3d4f47191e85bea0330cec99cc60110b1a1
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-21 02:21:16 +0200

    converted hooks to named exports

--- src/app/hooks/useToggle.jsx
+++ src/app/hooks/useToggle.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 
-const useToggle = () => {
+export const useToggle = () => {
   const [isToggled, setIsToggled] = useState(false);
 
   const toggle = () => setIsToggled(!isToggled);
 
   return { isToggled, toggle };
 };
-
-export default useToggle;
