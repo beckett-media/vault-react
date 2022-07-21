a5b871f3a81a8042210f11f179a8c785cf98c193
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-21 20:38:38 +0200

    reverted itemCard

--- src/app/components/ItemCard/ItemCard.jsx
+++ src/app/components/ItemCard/ItemCard.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { trimString, formatPrice } from '../../utils/strings';
-import { getItem } from '../../services/items';
 
 import './ItemCard.scss';
 
 const ItemCard = ({ data }, props) => {
-  const [defaultItem, setDefaultItem] = useState({});
-
-  console.log(defaultItem);
-
-  useEffect(() => {
-    getItem().then((data) => setDefaultItem(data));
-  }, []);
-
   return (
     <div className={`item-card_component`}>
       <div className='item-card_layout'>
@@ -32,7 +23,17 @@ const ItemCard = ({ data }, props) => {
 };
 
 ItemCard.defaultProps = {
-  data: defaultItem,
+  data: {
+    id: 1,
+    ownerId: 1,
+    title: `1933 Goudey Babe Ruth PSA 3  Test for Really Long Title that will not fit on the card`,
+    description: `Baseball ipsum dolor sit amet bleeder butcher boy fastball. Range assist batters box southpaw hitter 1-2-3 hack check swing. Corner curve line drive pickoff slugging count practice practice assist. 4-bagger hall of fame hot dog butcher boy starter, plunked baseball swing 4-6-3. Pitchout perfect game baseball card walk off starting pitcher flyout sport. Out double play plate season steal foul pole 4-bagger.`,
+    img: `https://www.deanscards.com/images/Basic%20pages/Babe%20Ruth%201933%20Goudey%20PSA%203.JPG`,
+    imgRev: 'https://uploads.tapatalk-cdn.com/20180529/ddf1bbba0522b0a64945e765b2b2df4c.jpg',
+    date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7).toString(),
+    price: 1200,
+    grade: 7.5,
+  },
 };
 
 export default ItemCard;
