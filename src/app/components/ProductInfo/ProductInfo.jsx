3d540ff9fb371d6ecdc3d1761af67ab979d59973
sDoddBeckett <SDodd@beckett.com>
2022-07-22 12:15:54 -0500

    updates to market wiring

--- src/app/components/ProductInfo/ProductInfo.jsx
+++ src/app/components/ProductInfo/ProductInfo.jsx
@@ -1,51 +1,105 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 import './ProductInfo.scss';
 
 import { formatPrice } from '../../utils/strings';
+import { createItemListing } from '../../services/items';
+import { getUser } from '../../services/user';
 
-const ProductInfo = ({ isOwner, item, addToCart }, props) => {
+const ProductInfo = ({ isOwner, item, addToCart }) => {
+
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    getUser().then((data) => setUser(data));
+  }, []);
+
+  const [ listItemInitiated, setlistItemInitiated ] = useState(false)
+  const [ name, setName ] = useState(item.title)
+  const [ price, setPrice ] = useState(item.price)
+  // TODO: Update items to include tags.
+  const [ newTag, setNewTag ] = useState('')
+
+
+  const listItem = () => {
+    if(item.name !== name){
+      updateItem({...item, title: name})
+    }
+    createItemListing({ vaulting_id: item.id, user: user.id, price: price}).then(res => console.log(res.data))
+  }
   return (
     <div className='product-info_component'>
-      <div className='product-info_title'>{item.title}</div>
-      <div className='product-info_stats'>
-        <div>{formatPrice(item.price)}</div>
-        <div>{item.grade ? item.grade : 'Beckett 10'}</div>
-      </div>
-      {item.tags && (
-        <div className='product-info_tags-wrapper'>
-          {item.tags.map((tag, index) => (
-            <div className='tag_component' key={index}>
-              {tag}
+      {
+        listItemInitiated ?
+        ( <Form>
+          <Form.Group>
+            <Form.Label>Card Name</Form.Label>
+            <Form.Control value={name} onChange={e => setName(e.target.value)}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Price</Form.Label>
+            <Form.Control value={price} onChange={e => setPrice(e.target.value)}/>
+          </Form.Group>
+          {/* TODO: We need to add tags to the items. */}
+          {/* <Form.Group>
+            <Form.Label>Add Tag</Form.Label>
+            <Form.Control onChange={e => setNewTag(e.target.value)}/>
+          </Form.Group> 
+          <div className='product-info_tags-wrapper'>
+            {item.tags.map((tag, index) => (
+              <div className='tag_component' key={index}>
+                {tag}
+              </div>
+            ))}
+          </div>*/}
+          <Button className='w-100' onClick={() => {listItem()}}>
+            Save
+          </Button>
+          <Button className='w-100' variant='outline-dark' onClick={() => addToCart()}>
+            Cancel
+          </Button>
+        </Form> ) : (
+        <>
+          <div className='product-info_title'>{item.title}</div>
+          <div className='product-info_stats'>
+            <div>{formatPrice(item.price)}</div>
+            <div>{item.grade ? item.grade : 'Beckett 10'}</div>
+          </div>
+          {item.tags && (
+            <div className='product-info_tags-wrapper'>
+              {item.tags.map((tag, index) => (
+                <div className='tag_component' key={index}>
+                  {tag}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          )}
+          <div className='product-info_buttons-wrapper'>
+            {isOwner ? (
+              <>
+                <Button className='w-100' onClick={() => setlistItemInitiated(true)}>
+                  Sell in Marketplace
+                </Button>
+                <Button className='w-100' variant='outline-dark' onClick={() => addToCart()}>
+                  Edit Card Details
+                </Button>
+                <Button className='text-start text-decoration-none' variant='link'>
+                  Remove From Beckett Vault
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button className='w-100' onClick={() => addToCart()}>
+                  Buy Now
+                </Button>
+                <Button className='w-100' variant='outline-dark' onClick={() => addToCart()}>
+                  Add To Cart
+                </Button>
+              </>
+            )}
+          </div>
+        </>
       )}
-      <div className='product-info_buttons-wrapper'>
-        {isOwner ? (
-          <>
-            <Button className='w-100' onClick={() => addToCart()}>
-              Sell in Marketplace
-            </Button>
-            <Button className='w-100' variant='outline-dark' onClick={() => addToCart()}>
-              Edit Card Details
-            </Button>
-            <Button className='text-start text-decoration-none' variant='link'>
-              Remove From Beckett Vault
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button className='w-100' onClick={() => addToCart()}>
-              Buy Now
-            </Button>
-            <Button className='w-100' variant='outline-dark' onClick={() => addToCart()}>
-              Add To Cart
-            </Button>
-          </>
-        )}
-      </div>
     </div>
   );
 };
