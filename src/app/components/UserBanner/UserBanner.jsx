1c0bf091c4e1214397c4b308269e4c640c0ad581
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-25 20:02:50 +0200

    fixed bg-image trouble

--- src/app/components/UserBanner/UserBanner.jsx
+++ src/app/components/UserBanner/UserBanner.jsx
@@ -15,8 +15,8 @@ const UserBanner = () => {
 
   return (
     <div className='user-banner_component'>
-      <BgSphere className='user-banner_bg-sphere' />
-      <div className='page-padding'>
+      <BgSphere className='user-banner_bg-sphere z-index-0' />
+      <div className='page-padding z-index-1 position-relative'>
         <div className='container-large'>
           <div className='user-banner_layout'>
             <div className='user-banner_image-wrapper'>
