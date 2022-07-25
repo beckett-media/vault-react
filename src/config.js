5986ef8501a23213496aecdb65b4083d0d9ba7c7
apogiatzis <apoyiatzis@outlook.com>
2022-07-25 18:55:56 +0300

    fix(devops): config env name

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import configData from './config.json';
 // use the general config data, and override it with server specific variables
 // todo: add other servers qa, stage, dev
 let config;
-if (process.env.REACT_APP_ENV === 'production') {
+if (process.env.REACT_APP_ENV === 'prod') {
   config = Object.assign({}, configData.general, configData.production);
 } else if (process.env.REACT_APP_ENV === 'qa') {
   config = Object.assign({}, configData.general, configData.qa);
