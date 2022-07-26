45b916339bbeedb6738dcf26d9dc1206ccbf1fe2
sDoddBeckett <SDodd@beckett.com>
2022-07-26 08:24:55 -0500

    removed line space

--- src/app/services/submission.js
+++ src/app/services/submission.js
@@ -41,7 +41,6 @@ export const getSubmissions = async ({ user, status, offset, limit, order } = {}
   return mockItems.slice(0, 8);
 };
 
-
 export const getSingleSubmission = async (submissionId) => {
   return axios.get(`${config.BASE_URL}/marketplace/submission/${submissionId}`).then((res) => {
     return res.data;
