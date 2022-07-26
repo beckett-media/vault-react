8b4d1d6aecdb826976acaff5179927f19171ddb9
sDoddBeckett <SDodd@beckett.com>
2022-07-26 08:21:29 -0500

    updates per comments

--- src/app/pages/History/History.js
+++ src/app/pages/History/History.js
@@ -15,7 +15,6 @@ const History = () => {
       setUser(userObject);
       getHistory(user.name)
         .then((res) => {
-          console.log(res)
           if (res.status === 200) {
             if (res.data.length !== 0) {
               setHistoryItems(res.data);
