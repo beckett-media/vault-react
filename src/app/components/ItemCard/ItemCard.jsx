3bb25027e79671cddcb644816936c7bc2060bd55
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-28 23:38:53 +0200

    Updated styling to match Figma

--- src/app/components/ItemCard/ItemCard.jsx
+++ src/app/components/ItemCard/ItemCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { formatPrice, trimString } from '../../utils/strings';
+
 import './ItemCard.scss';
 
-const ItemCard = ({ item, shouldLink = true }, props) => {
+import { formatPrice, trimString } from '../../utils/strings';
+
+const ItemCard = ({ item, shouldLink = true, belongsToUser }, props) => {
   const link = shouldLink ? `/item/${item.id}` : '';
   const price = item.est_value || item.price;
   const images = require.context('../../assets/Images', true);
@@ -15,13 +17,20 @@ const ItemCard = ({ item, shouldLink = true }, props) => {
             <img className='item-card_image' src={images(`./${item.image_url}`)} alt='' />
           </div>
           <div className='item-card_content-wrapper'>
-            <div className='item-card_title'>{trimString(item.title, 20)}</div>
-            <div className='item-card_price'>{formatPrice(+price)}</div>
+            <div className='item-card_category'>{formatPrice(+price)}</div>
+            {!belongsToUser && <div className='item-card_price'>{formatPrice(+price)}</div>}
+            <div className='item-card_title ellipses_wrapper'>
+              <span className='ellipses_child'>{item.title}</span>
+            </div>
           </div>
         </Link>
       </div>
     </div>
   );
+};
+
+ItemCard.defaultProps = {
+  belongsToUser: false,
 };
 
 export default ItemCard;
