a0cd7ab037056b3610352f579c8a487ca856f8e7
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-07-29 20:04:18 +0200

    updated and restyled nav based on Figma

--- src/app/components/Generic/TopNav.js
+++ src/app/components/Generic/TopNav.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
-import { Nav, Navbar, NavDropdown, Container, Button } from 'react-bootstrap';
-import './Nav.scss';
-import { AuthContext } from '../../contexts/auth';
-import { useCartContext } from '../../contexts/cart';
 import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom';
+import { NavDropdown } from 'react-bootstrap';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+
+import './TopNav.scss';
+
+import { AuthContext } from '../../contexts/auth';
+import { useCartContext } from '../../contexts/cart';
 
 const TopNav = () => {
   const authContext = useContext(AuthContext);
@@ -19,80 +21,78 @@ const TopNav = () => {
   };
 
   return (
-    <Navbar bg='dark' variant='dark' expand='lg' fixed='top'>
-      <Container>
-        <Navbar.Brand>
-          <Link to='/'>
-            <img src={require('../../assets/beta-logo.png')} className='nav_logo' />
-          </Link>
-        </Navbar.Brand>
-        {!isSigninPage() && <Navbar.Toggle aria-controls='basic-navbar-nav' />}
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='m-auto'>
-            {authContext.isSignedIn && (
-              <>
-                {/* <NavLink to='/about' className={({ isActive }) => (isActive ? 'active-nav m-2' : 'm-2')}>
+    <div className='nav_component'>
+      <div className='page-padding'>
+        <div className='container-large'>
+          <div className='nav_layout'>
+            <div className='nav_logo-wrapper'>
+              <Link to='/'>
+                <img src={require('../../assets/beta-logo.png')} className='nav_logo' />
+              </Link>
+            </div>
+            {/* {!isSigninPage() && <Navbar.Toggle aria-controls='basic-navbar-nav' />} */}
+            <div className='nav_center-wrapper'>
+              {authContext.isSignedIn && (
+                <>
+                  {/* <NavLink to='/about' className={({ isActive }) => (isActive ? 'active-nav m-2' : 'm-2')}>
                   About Vault
                 </NavLink> */}
-                <NavLink to='/collection' className={({ isActive }) => (isActive ? 'active-nav m-2' : 'm-2')}>
-                  My Collection
-                </NavLink>
-                <NavLink to='/market' className={({ isActive }) => (isActive ? 'active-nav m-2' : 'm-2')}>
-                  Marketplace
-                </NavLink>
-              </>
-            )}
-          </Nav>
-          <Nav className='ml-auto'>
-            {authContext.isSignedIn && (
-              <OverlayTrigger
-                delay={{ hide: 450, show: 300 }}
-                overlay={(props) => <Tooltip {...props}>Coming Soon!</Tooltip>}
-                placement='bottom'
-              >
-                <Button size='sm' className='bg submit-nav' variant='primary'>
-                  Submit Item
-                </Button>
-              </OverlayTrigger>
-            )}
-            {!isSigninPage() && (
-              <NavDropdown title={<i className='fa-solid fa-user'></i>} id='basic-nav-dropdown'>
-                {authContext.isSignedIn ? (
-                  <>
-                    <NavDropdown.Item>
-                      <Link to='/profile'>Profile</Link>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <Link to='/history'>History</Link>
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item
+                  <NavLink to='/collection' className={({ isActive }) => (isActive ? 'nav_link--active' : 'nav_link')}>
+                    My Collection
+                  </NavLink>
+                  <NavLink to='/market' className={({ isActive }) => (isActive ? 'nav_link--active' : 'nav_link')}>
+                    Marketplace
+                  </NavLink>
+                </>
+              )}
+            </div>
+            <div className='nav_end-wrapper'>
+              {authContext.isSignedIn && (
+                <OverlayTrigger
+                  delay={{ hide: 450, show: 300 }}
+                  overlay={(props) => <Tooltip {...props}>Coming Soon!</Tooltip>}
+                  placement='bottom'
+                >
+                  <div className='nav_button'>Add Item</div>
+                </OverlayTrigger>
+              )}
+              {!isSigninPage() && (
+                <div className='nav_user-dropdown'>
+                  <div className='nav_user-dropdown-button'>
+                    <i className='nav_user-dropdown-button-icon fa-solid fa-user'> </i>
+                  </div>
+                  <div className='nav_user-dropdown-list'>
+                    <Link className='nav_user-dropdown-item' to='/profile'>
+                      Profile
+                    </Link>
+                    <Link className='nav_user-dropdown-item' to='/history'>
+                      History
+                    </Link>
+                    <hr />
+                    <div
+                      className='nav_user-dropdown-item'
                       onClick={async () => {
                         authContext.signOut();
                         navigate('/');
                       }}
                     >
                       Logout
-                    </NavDropdown.Item>
-                  </>
-                ) : (
-                  <NavDropdown.Item>
-                    <Link to='/signin'>Login</Link>
-                  </NavDropdown.Item>
-                )}
-              </NavDropdown>
-            )}
-            {cartItemsLength ? (
-              <Link to='/cart'>
-                <i className='fa-solid fa-cart-shopping mt-2 p-1'></i>
-              </Link>
-            ) : (
-              <></>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {cartItemsLength ? (
+                <Link to='/cart'>
+                  <i className='fa-solid fa-cart-shopping mt-2 p-1'></i>
+                </Link>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
