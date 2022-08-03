5cc18cf2368d9bb281291950d6835b6631477c3a
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-08-04 01:38:29 +0200

    continued building out response component

--- src/app/pages/Submission/SubmissionResponse.js
+++ src/app/pages/Submission/SubmissionResponse.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { ReactComponent as SuccessSVG } from '../../assets/beckett-check-icon.svg';
+import { ReactComponent as ErrorSVG } from '../../assets/beckett-error-icon.svg';
 
 const SubmissionResponse = ({ submissionResponse, setSubmissionResponse }) => {
+  // const response = JSON.parse(submissionResponse);
+  const response = { statusCode: 404, message: 'Submission not found', error: 'Not Found' };
+
+  console.log(response);
+
+  const success = response.statusCode === 200;
+  const image = success ? (
+    <SuccessSVG className='submission_response-image' />
+  ) : (
+    <ErrorSVG className='submission_response-image' />
+  );
+  const heading = success ? 'Success!' : 'Error';
+  const body = success
+    ? 'Your card has been submitted.'
+    : `${response.statusCode}, ${response.message.toLocaleLowerCase()}.`;
+
   return (
     <div className='w-100 h-100'>
       <div className='submission_form'>
         <div className='submission_form-section'>
           <div className='submission_form-response'>
-            <div className='submission_reponse-image-wrapper'></div>
-            <div className='submission_reponse-body-wrapper'>{submissionResponse}</div>
+            <div className='submission_response-image-wrapper'>{image}</div>
+            <div className='submission_response-body-wrapper'>
+              <div className='submission_response-heading'>{heading}</div>
+              <div className='submission_response-body'>{body}</div>
+            </div>
             <Button onClick={() => setSubmissionResponse('')}>OK</Button>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+SubmissionResponse.defaults = {
+  submissionResponse: { statusCode: 404, message: 'Submission not found', error: 'Not Found' },
 };
 
 export default SubmissionResponse;
