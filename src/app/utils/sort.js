c2f2ef7a05fcc05be71b76df21f2b83d38380c78
Dan <dbaumfeld@beckett.com>
2022-08-03 12:16:38 -0700

    chore: pr feedback

--- src/app/utils/sort.js
+++ src/app/utils/sort.js
@@ -2,17 +2,17 @@ import { isObject } from './objects';
 export const ASC = 'asc';
 export const DESC = 'desc';
 
+const sortValue = (a, b, dir) => {
+  const adjustor = dir === ASC ? 1 : -1;
+  if (a < b) return -1 * adjustor;
+  if (a > b) return 1 * adjustor;
+  return 0;
+};
+
 export const sortByAttribute = (attribute, dir = ASC) => {
   // asc or desc
   return (aObj, bObj) => {
     if (!isObject(aObj) || !isObject(bObj)) return 0;
     return sortValue(aObj[attribute], bObj[attribute], dir);
   };
-};
-
-const sortValue = (a, b, dir) => {
-  const adjustor = dir === ASC ? 1 : -1;
-  if (a < b) return -1 * adjustor;
-  if (a > b) return 1 * adjustor;
-  return 0;
 };
