d750c0a9ea0c10e3af030bbd1e4308d5359dde60
Louis de Tilly <louisdtb@gmail.com>
2022-08-11 02:28:28 +0200

    created and set up order get request

--- src/app/pages/OrderDetails/OrderPrint.jsx
+++ src/app/pages/OrderDetails/OrderPrint.jsx
@@ -1,16 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import { getOrders } from '../../services/order';
+
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
 const OrderPrint = () => {
-    const [order, setOrder] = useState();
+    const [order, setOrder] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
     const {orderId} = useParams();
 
     useEffect(() => {
-        // TODO: get order data.
-      }, [orderId]);
+      setIsLoading(true)
+      getOrders(orderId).then(resp => setOrder(resp)).catch(e => console.log(e))
+
+    }, [orderId]);
 
   return (
-    <div>{orderId}</div>
+    <div>
+      {isLoading && (<LoadingSpinner/>)}
+      {order && 'test'}
+    </div>
   )
 }
 
