e16b59a2a030ecccd4ee3b5825b159186fc5b9c7
Louis de Tilly <louisdtb@gmail.com>
2022-08-12 01:24:41 +0200

    added error handling to order details

--- src/app/pages/OrderDetails/OrderPrint.jsx
+++ src/app/pages/OrderDetails/OrderPrint.jsx
@@ -1,32 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import { getOrders } from '../../services/order';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { getSingleOrder } from '../../services/order';
 
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
 const OrderPrint = () => {
-    const [order, setOrder] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+  const [order, setOrder] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    const {orderId} = useParams();
+  const { orderId } = useParams();
 
-    useEffect(() => {
-      setIsLoading(true)
-      getOrders(orderId).then(resp => setOrder(resp)).catch(e => console.log(e))
+  useEffect(() => {
+    setIsLoading(true);
+    getSingleOrder(orderId)
+      .then((resp) => setOrder(resp))
+      .then(setIsLoading(false))
+      .catch((e) => {
+        console.log('error');
+        setError(true);
+      });
+  }, [orderId]);
 
-    }, [orderId]);
+  console.log(order);
 
   return (
     <div>
       {isLoading && (
-      <div className='d-flex flex-column align-items-center'>
-      <LoadingSpinner/> 
-      <div>order loading</div>
-      </div>
+        <div className='d-flex flex-column align-items-center'>
+          <LoadingSpinner />
+          <div>order loading</div>
+        </div>
+      )}
+      {error && (
+        <div className='d-flex flex-column align-items-center'>
+          <div>There was an error retrieving this order.</div>
+          <Button onClick={() => setError(false)}>OK</Button>
+        </div>
       )}
       {order && 'test'}
     </div>
-  )
-}
+  );
+};
 
-export default OrderPrint
+export default OrderPrint;
