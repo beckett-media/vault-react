49fbfa536a2785f780ab5a2827825619f450f278
anthonyplain <anthonyplain@users.noreply.github.com>
2022-08-12 11:21:52 -0400

    chore: pr feedback

--- src/app/pages/Admin/SubmissionItem.js
+++ src/app/pages/Admin/SubmissionItem.js
@@ -14,27 +14,12 @@ function SubmissionItem({ onApprove, onReject, item }) {
 
   return (
     <div className='m-4'>
-      {/* <Row>
-        <Col className='info-box'>
-          <p>
-            {item.genre} - {item.title}
-          </p>
-        </Col>
-        <Col className='right-align'>{item.manufacturer}</Col>
-      </Row> */}
       <Row className='mt-4 mb-2'>
         <ItemCard item={item} shouldLink={false} belongsToUser={false} />
         <p>
           Status: <b>{item.status_desc}</b>
         </p>
       </Row>
-      {/* <Row>
-        <p>{item.description}</p>
-        <p>{`Company: ${item.grading_company}, Serial number: ${item.serial_number}`}</p>
-        <p>{`Year: ${item.year}, Estimated value: $${item.est_value}`}</p>
-        <p>{`Overall grade: ${item.overall_grade}, Sub grades: ${item.sub_grades}`}</p>
-        <p>{`Autograph: ${item.autograph}, Item uuid: ${item.item_uuid}`}</p>
-      </Row> */}
       <div className='mt-4'>
         <SubmitButton
           func={() => onApprove(item.id)}
