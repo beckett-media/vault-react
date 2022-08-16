c0a8736121d1a8029cc6f18aaba686ca565c37f4
sDoddBeckett <SDodd@beckett.com>
2022-08-16 11:19:43 -0500

    added tests and fixed an import

--- src/app/pages/Admin/_test/Admin.test.js
+++ src/app/pages/Admin/_test/Admin.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import History from '../History';
+import AdminPage from '../AdminPage'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const root = ReactDOM.createRoot(div);
-  root.render(<History />);
+  root.render(<AdminPage />);
 });
--- src/app/pages/Settings/_test/Settings.test.js
+++ src/app/pages/Settings/_test/Settings.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import History from '../History';
+import Settings from '../Settings'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const root = ReactDOM.createRoot(div);
-  root.render(<History />);
+  root.render(<Settings />);
 });
