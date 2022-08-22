d1f2e75b43074d75cd329b5f1912b7f1b22217bd
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-08-22 23:14:12 +0200

    added page styling

--- src/app/pages/OrderDetails/OrderDetails.jsx
+++ src/app/pages/OrderDetails/OrderDetails.jsx
@@ -24,16 +24,9 @@ const OrderDetails = () => {
             </div>
           </div>
           <div className='mt-4 w-100 d-flex flex-column align-items-center'>
-            <Col lg={6} className='text-center mb-2'>
+            <Col lg={6} className='text-center mb-4'>
               To send your order to Beckett Vault, please click the button below to print your order confirmation and
-              include it in your shipping box.
-            </Col>
-            <Col lg={6} sm={12} className='text-center mb-4'>
-              <strong>Ship orders to:</strong>
-              <div>Beckett Collectibles</div>
-              <div>C/O Beckett Vault</div>
-              <div>2700 Summit Ave, Ste 100</div>
-              <div>Plano, TX 75074</div>
+              <strong> include it in your shipping box.</strong>
             </Col>
             <Button className='mb-4' onClick={() => printSection('print-area')}>
               Print order form
