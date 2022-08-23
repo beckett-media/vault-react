ae9c16a057fe367879bf9f0c4d619e2e2ed8d261
anthonyplain <anthonyplain@users.noreply.github.com>
2022-08-23 15:29:38 -0400

    chore: require issue field

--- src/app/pages/Submission/SubmissionAdd.js
+++ src/app/pages/Submission/SubmissionAdd.js
@@ -176,6 +176,7 @@ const SubmissionAdd = ({ submitAddedItem }) => {
                       <Form.Label>Issue #*</Form.Label>
                       <Form.Control
                         type='text'
+                        required
                         onChange={(e) => updateItem({ issue: e.target.value.replace(/#/g, '') })}
                       />
                     </Form.Group>
