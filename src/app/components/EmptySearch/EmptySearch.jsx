b1012e35cfb17965a22b97a558d5e59953fd9c51
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-08-25 21:39:06 +0200

    added search clear functionality

--- src/app/components/EmptySearch/EmptySearch.jsx
+++ src/app/components/EmptySearch/EmptySearch.jsx
@@ -5,14 +5,18 @@ import './EmptySearch.scss';
 
 import { ReactComponent as SearchIcon } from '../../assets/empty-search.svg';
 
-const EmptySearch = ({ searchTerm }) => {
+const EmptySearch = ({ searchTerm, clearFunction }) => {
   return (
     <div className='empty-search_component'>
       <SearchIcon className='empty-search_icon' />
       <div className='empty-search_heading'>No Results Found</div>
       <div className='empty-search_body'>Your search “{searchTerm}” did not match any items. Please try again.</div>
       <div className='empty-search_button-wrapper'>
-        <Button variant='outline-primary'>Clear Search</Button>
+        {clearFunction && (
+          <Button variant='outline-primary' onClick={() => clearFunction()}>
+            Clear Search
+          </Button>
+        )}
         <Link to='/submission'>
           <Button>+ Add Item</Button>
         </Link>
