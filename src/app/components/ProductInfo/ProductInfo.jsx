6339c95329b7916df909f44e20c396c609aba65e
sDoddBeckett <SDodd@beckett.com>
2022-08-25 21:37:21 -0500

    removed typo

--- src/app/components/ProductInfo/ProductInfo.jsx
+++ src/app/components/ProductInfo/ProductInfo.jsx
@@ -76,7 +76,7 @@ const ProductInfo = ({ isOwner, item, addToCart }) => {
             <div>{formatPrice(price)}</div>
           </div>
           <div className='product-info_stats'>
-            <div>{item.overall_grade  && `Grade: ,${item.overall_grade}`}</div>
+            <div>{item.overall_grade  && `Grade: ${item.overall_grade}`}</div>
           </div>
           {item.tags && (
             <div className='product-info_tags-wrapper'>
