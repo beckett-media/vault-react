23296f4007eadb01862727a134614b4e8094f0d0
sDoddBeckett <SDodd@beckett.com>
2022-08-26 14:15:41 -0500

    updated to ternary and removed length check

--- src/app/components/ListItem/ListItem.jsx
+++ src/app/components/ListItem/ListItem.jsx
@@ -29,7 +29,7 @@ const ListItem = ({ item }, props) => {
             <div className='ellipses_child'>{item.description?.length && item.description}</div>
           </div>
 
-          <div>{item.grade?.length && !isNaN(item.grade) && item.grade}</div>
+          <div>{(!isNaN(item.grade)) ? item.grade: ''}</div>
           <div>{formatPrice(+item.est_value)}</div>
         </div>
       </Link>
