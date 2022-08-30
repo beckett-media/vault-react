935225c2c01fb4ad798368488f5f88d35839c822
Louis de Tilly <louisdtb@gmail.com>
2022-08-30 20:39:15 +0200

    implemented status tracker logic

--- src/app/components/StatusTracker/StatusTracker.jsx
+++ src/app/components/StatusTracker/StatusTracker.jsx
@@ -2,26 +2,41 @@ import React from 'react';
 
 import './StatusTracker.scss';
 
-import { ReactComponent as StatusBeginning } from '../../assets/StatusBeginning.svg';
-import { ReactComponent as StatusMiddle } from '../../assets/StatusMiddle.svg';
-import { ReactComponent as StatusEnd } from '../../assets/StatusEnd.svg';
+const StatusTracker = ({ totalSteps = 2, currentStep = 1 }) => {
+  if (totalSteps < 2) {
+    totalSteps = 2;
+  }
 
-const StatusTracker = ({ length = 0, position = 1 }) => {
-  const steps = length > 2 ? Array.from(Array(length - 2).keys()) : [];
+  if (currentStep > totalSteps) {
+    currentStep = totalSteps;
+  } else if (currentStep < 1) {
+    currentStep = 1;
+  }
 
-  console.log(steps);
-  console.log(steps.length);
+  const middleStepsArray = totalSteps > 2 ? Array(totalSteps - 2).fill(0) : [];
+  const activeMiddleSteps = currentStep - 1;
+
+  for (let index = 0; index < activeMiddleSteps; index++) {
+    middleStepsArray[index] = 1;
+  }
+
+  // 1. Create mapping array for all middle steps
+  // 2. For all items between the first and the active step, update the value to 1
+
+  console.log(middleStepsArray);
+  console.log(middleStepsArray.totalSteps);
   return (
     <div className='status-tracker_component'>
       <div className='status-tracker_element status-tracker_beginning'></div>
-      {steps.length > 0 &&
-        steps.map((item) => (
-          <div
-            className='status-tracker_element status-tracker_middle'
-            key={`status-tracker_${Math.random() * 1000}`}
-          ></div>
-        ))}
-      <div className='status-tracker_element status-tracker_end'></div>
+      {middleStepsArray.map((item) => (
+        <div
+          className={`status-tracker_element status-tracker_middle ${item === 1 && 'status-tracker_active'}`}
+          key={`status-tracker_${Math.random() * 1000}`}
+        ></div>
+      ))}
+      <div
+        className={`status-tracker_element status-tracker_end ${totalSteps === currentStep && 'status-tracker_active'}`}
+      ></div>
     </div>
   );
 };
