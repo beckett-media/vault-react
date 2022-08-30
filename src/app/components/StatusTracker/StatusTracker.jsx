045256869c1b7b3ec425c385f51b2cb07726d696
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-08-30 23:35:46 +0200

    added error icons and logic

--- src/app/components/StatusTracker/StatusTracker.jsx
+++ src/app/components/StatusTracker/StatusTracker.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import './StatusTracker.scss';
 
+import { ReactComponent as CrossIcon } from '../../assets/beckett-circle-cross.svg';
+import { ReactComponent as WarningIcon } from '../../assets/beckett-cirlce-warning.svg';
+
 const StatusTracker = ({ totalSteps = 2, currentStep = 1 }) => {
   if (totalSteps < 2) {
     totalSteps = 2;
@@ -10,7 +13,7 @@ const StatusTracker = ({ totalSteps = 2, currentStep = 1 }) => {
   if (currentStep > totalSteps) {
     currentStep = totalSteps;
   } else if (currentStep < 1) {
-    currentStep = 1;
+    currentStep = 0;
   }
 
   const middleStepsArray = totalSteps > 2 ? Array(totalSteps - 2).fill(0) : [];
@@ -27,16 +30,24 @@ const StatusTracker = ({ totalSteps = 2, currentStep = 1 }) => {
   console.log(middleStepsArray.totalSteps);
   return (
     <div className='status-tracker_component'>
-      <div className='status-tracker_element status-tracker_beginning'></div>
-      {middleStepsArray.map((item) => (
-        <div
-          className={`status-tracker_element status-tracker_middle ${item === 1 && 'status-tracker_active'}`}
-          key={`status-tracker_${Math.random() * 1000}`}
-        ></div>
-      ))}
-      <div
-        className={`status-tracker_element status-tracker_end ${totalSteps === currentStep && 'status-tracker_active'}`}
-      ></div>
+      {!!currentStep ? (
+        <>
+          <div className='status-tracker_element status-tracker_beginning'></div>
+          {middleStepsArray.map((item) => (
+            <div
+              className={`status-tracker_element status-tracker_middle ${item === 1 && 'status-tracker_active'}`}
+              key={`status-tracker_${Math.random() * 1000}`}
+            ></div>
+          ))}
+          <div
+            className={`status-tracker_element status-tracker_end ${
+              totalSteps === currentStep && 'status-tracker_active'
+            }`}
+          ></div>
+        </>
+      ) : (
+        <CrossIcon className='status-tracker_error-icon'></CrossIcon>
+      )}
     </div>
   );
 };
