e3f95121afe3279f5b9f76310cffccb90a8c6b31
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-08-31 16:50:08 +0200

    installed tag manager package and added to app.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './app/components/ScrollToTop/ScrollToTop';
 
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-MKJ4MQF',
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
