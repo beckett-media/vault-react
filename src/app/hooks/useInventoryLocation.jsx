aef6601ddfdbe68bb4dfb1229e3e4f36b82898fa
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-02 01:05:56 +0200

    Create useInventoryLocation.jsx

---
+++ src/app/hooks/useInventoryLocation.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { getInventory, postInventory, putInventory } from '../services/inventory';
+
+const blankLocation = {
+  vault: '',
+  zone: '',
+  shelf: '',
+  row: '',
+  box: '',
+  slot: '',
+};
+
+export const useInventoryLocation = (itemId) => {
+  const [apiRetrigger, setApiRetrigger] = useState({});
+  const [initialInventory, setInitialInventory] = useState({});
+  const [inventory, setInventory] = useState(blankLocation);
+
+  useEffect(() => {
+    getInventory({ item_ids: [itemId] })
+      .then((data) => {
+        setInitialInventory(...data);
+      })
+      .catch(console.log('failed to retrieve inventory'));
+  }, [apiRetrigger]);
+
+  const updateInventory = (tempInventory) => setInventory({ ...inventory, ...tempInventory });
+
+  const locationFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(inventory);
+
+    if (!!initialInventory) {
+      putInventory(itemId, inventory)
+        .then((resp) => console.log('success!'))
+        .catch((e) => console.log(e));
+      setApiRetrigger({});
+    } else {
+      inventory.item_id = item.item_id;
+      postInventory(inventory)
+        .then((resp) => console.log('success!'))
+        .catch((e) => console.log(e));
+      setApiRetrigger({});
+    }
+  };
+
+  return { inventory, initialInventory, updateInventory, locationFormSubmit };
+};
