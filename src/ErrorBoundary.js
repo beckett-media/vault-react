3b0d3a48c579f851f0fad546f9e889633ce35714
sDoddBeckett <SDodd@beckett.com>
2022-09-06 13:48:30 -0500

    updated style and link

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
-
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
@@ -13,13 +14,21 @@ class ErrorBoundary extends React.Component {
 
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
-    logErrorToMyService(error, errorInfo);
+    // logErrorsHere
   }
 
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div className='error-boundary'>
+          Something went wrong. Please refresh the page or &nbsp;
+          <Link className='home-button' to='my-collection'>
+            go home
+          </Link>
+          .
+        </div>
+      );
     }
 
     return this.props.children;
