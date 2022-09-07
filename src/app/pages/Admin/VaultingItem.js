9bd98e304ba7d42cfc74ebe9af1ac2d2b31903d8
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-07 19:14:10 +0200

    cleaned up vaulting page with inventory hook logic

--- src/app/pages/Admin/VaultingItem.js
+++ src/app/pages/Admin/VaultingItem.js
@@ -2,74 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import SubmitButton from '../../components/Generic/SubmitButton';
 import { VAULTING_STATUS } from '../../services/items';
-import { getInventory, postInventory, putInventory } from '../../services/inventory';
+import { useInventoryLocation } from '../../hooks/useInventoryLocation';
+import { getInventory, postInventory, putInventory, getInventoryZoneOptions } from '../../services/inventory';
 import './VaultingItem.scss';
-
-const zoneOptions = [
-  'Cabinet 1',
-  'Cabinet 2',
-  'Cabinet 3',
-  'Cabinet 4',
-  'Cabinet 5',
-  'Cabinet 6',
-  'Cabinet 7',
-  'Cabinet 8',
-  'Credenza 1',
-  'Credenza 2',
-  'Credenza 3',
-  'Credenza 4',
-  'Credenza 5',
-  'Credenza 6',
-  'Credenza 7',
-  'Credenza 8',
-  'Main Display Case',
-  'Pedestal 1',
-  'Pedestal 2',
-  'Comics Gallery Wall',
-  'Card Gallery Wall',
-];
-
-const blankLocation = {
-  vault: '',
-  zone: '',
-  shelf: '',
-  row: '',
-  box: '',
-  slot: '',
-};
+import { blankLocation } from '../../const/inventory';
 
 function VaultingItem({ onWithdraw, item }) {
   const shouldEnableWithdrawButton = item.status === VAULTING_STATUS.Minted;
-  const [apiRetrigger, setApiRetrigger] = useState({});
-  const [initialInventory, setInitialInventory] = useState({});
-  const [inventory, setInventory] = useState(blankLocation);
-
-  useEffect(() => {
-    getInventory({ item_ids: [item.item_id] })
-      .then((data) => {
-        setInitialInventory(...data);
-      })
-      .catch(console.log('failed to retrieve inventory'));
-  }, [apiRetrigger]);
-
-  const updateInventory = (tempInventory) => setInventory({ ...inventory, ...tempInventory });
-
-  const locationFormSubmit = (e) => {
-    e.preventDefault();
-
-    if (initialInventory) {
-      putInventory(initialInventory.id, inventory)
-        .then()
-        .catch((e) => console.log(e));
-      setApiRetrigger({});
-    } else {
-      inventory.item_id = item.item_id;
-      postInventory(inventory)
-        .then()
-        .catch((e) => console.log(e));
-      setApiRetrigger({});
-    }
-  };
+  const { currentLocation } = useInventoryLocation(item.item_id);
 
   return (
     <div className='m-4'>
@@ -115,94 +55,12 @@ function VaultingItem({ onWithdraw, item }) {
         <p>
           <span className='fw-bold'>Item uuid:</span> {item.item_uuid}
         </p>
-      </Row>
-      <Row className='mt-4'>
-        <div>
-          <span className='fw-bold'>Vault location: </span>
-          {initialInventory ? initialInventory.label : 'No inventory location set'}
-        </div>
-        <Form onSubmit={(e) => locationFormSubmit(e)} className='mt-2 mb-4'>
-          <Row>
-            <Col lg={4}>
-              <Form.Group>
-                <Form.Label>Vault</Form.Label>
-                <Form.Select onChange={(e) => updateInventory({ vault: e.target.value })}>
-                  <option> - Select -</option>
-                  <option value={'dallas'}>Dallas</option>
-                  <option value={'delaware'}>Delaware</option>
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col lg={4}>
-              <Form.Group>
-                <Form.Label>Zone</Form.Label>
-                <Form.Select onChange={(e) => updateInventory({ zone: e.target.value })}>
-                  <option>- Select -</option>
-                  {zoneOptions.map((item, index) => (
-                    <option key={`zone-area_${index}`} value={item.toLowerCase()}>
-                      {item}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
-          {(inventory?.zone?.toLowerCase().startsWith('credenza') ||
-            inventory?.zone?.toLowerCase().startsWith('cabinet')) && (
-            <Row>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Shelf</Form.Label>
-                  <Form.Select onChange={(e) => updateInventory({ shelf: e.target.value })}>
-                    <option>- Select -</option>
-                    <option value='1'>Shelf 1</option>
-                    <option value='2'>Shelf 2</option>
-                    <option value='3'>Shelf 3</option>
-                    <option value='4'>Shelf 4</option>
-                    <option value='5'>Shelf 5</option>
-                    <option value='6'>Shelf 6</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Box</Form.Label>
-                  <Form.Control onChange={(e) => updateInventory({ box: e.target.value })} />
-                </Form.Group>
-              </Col>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Slot</Form.Label>
-                  <Form.Control onChange={(e) => updateInventory({ slot: e.target.value })} />
-                </Form.Group>
-              </Col>
-            </Row>
-          )}
-          {inventory?.zone?.toLowerCase().endsWith('gallery wall') && (
-            <Row>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Row</Form.Label>
-                  <Form.Select onChange={(e) => updateInventory({ row: e.target.value })}>
-                    <option>- Select -</option>
-                    <option value='1'>Row 1</option>
-                    <option value='2'>Row 2</option>
-                    <option value='3'>Row 3</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Slot</Form.Label>
-                  <Form.Control onChange={(e) => updateInventory({ slot: e.target.value })} />
-                </Form.Group>
-              </Col>
-            </Row>
-          )}
-          <Button type='submit' className='mt-4'>
-            {initialInventory ? 'Update Location' : 'Add Location'}
-          </Button>
-        </Form>
+        <p>
+          <span className='fw-bold'>Item id:</span> {item.item_id}
+        </p>
+        <p>
+          <span className='fw-bold'>Vault location:</span> {currentLocation ? currentLocation.label : 'No location set'}
+        </p>
       </Row>
       <div className='mt-4'>
         <SubmitButton
