46823f6c1ee94ad1c401d62c0961f2f152d55950
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-08 21:38:17 +0200

    made getVaultingTitle function data source agnostic

--- src/app/utils/vaulting.js
+++ src/app/utils/vaulting.js
@@ -1,12 +1,23 @@
 import { ITEM_TYPE } from '../services/items';
 
-export const getVaultingTitle = (item) => {
-  if (item.item_type === ITEM_TYPE.TRADING_CARD) {
-    return `${item.year} ${item.set_name || ''} ${item.card_number ? '#' + item.card_number : ''} ${item.player}`;
-  }
+export const getTitle = (item, vaultingOrSubmission) => {
+  if (vaultingOrSubmission.toLowerCase() === 'vaulting') {
+    if (item.item_type === ITEM_TYPE.TRADING_CARD) {
+      return `${item.year} ${item.set_name || ''} ${item.card_number ? '#' + item.card_number : ''} ${item.player}`;
+    }
 
-  if (item.item_type === ITEM_TYPE.COMIC) {
-    return `${item.title} ${item.issue ? '#' + item.issue : ''} ${item.publisher} ${item.year}`;
+    if (item.item_type === ITEM_TYPE.COMIC) {
+      return `${item.title} ${item.issue ? '#' + item.issue : ''} ${item.publisher} ${item.year}`;
+    }
+  }
+  if (vaultingOrSubmission.toLowerCase() === 'submission') {
+    if (item.type === ITEM_TYPE.TRADING_CARD) {
+      return `${item.year} ${item.set_name || ''} ${item.card_number ? '#' + item.card_number : ''} ${item.player}`;
+    }
+
+    if (item.type === ITEM_TYPE.COMIC) {
+      return `${item.title} ${item.issue ? '#' + item.issue : ''} ${item.publisher} ${item.year}`;
+    }
   }
 
   return '';
