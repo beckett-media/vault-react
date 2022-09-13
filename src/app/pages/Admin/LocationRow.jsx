85eb0e267f1fd51a6df6a5861b91a37d7e887435
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-14 01:38:56 +0200

    customized LocationRow for use case

--- src/app/pages/Admin/LocationRow.jsx
+++ src/app/pages/Admin/LocationRow.jsx
@@ -13,7 +13,6 @@ const InventoryLocationForm = ({ itemId }) => {
   const [inventory, setInventory] = useState(blankLocation);
   const [isPostLoading, setIsPostLoading] = useState(false);
   const [isPutLoading, setIsPutLoading] = useState(false);
-  const [isAddingLocation, setIsAddingLocation] = useState(false);
   const [newLocationId, setNewLocationId] = useState();
 
   useEffect(() => {
@@ -72,152 +71,97 @@ const InventoryLocationForm = ({ itemId }) => {
   };
 
   return (
-    <div>
-      <div>
-        <span className='fw-bold'>Vault location: </span>
-        {currentLocation ? currentLocation?.label : 'No inventory location set'}
-      </div>
-      <div>
-        {initialInventory.length > 0 && (
-          <>
-            <span className='fw-bold'>Select another location: </span>
-            <ListGroup defaultActiveKey='#current'>
-              {initialInventory?.map((item, index) => (
-                <ListGroup.Item
-                  action
-                  key={'inventory-location_' + index}
-                  href={item.status === 1 ? '#current' : '#link' + index}
-                  onClick={() => setNewLocationId(item.id)}
-                >
-                  <div className='d-flex justify-content-between'>
-                    <span>{item?.label}</span>
-                    <span className='fw-bold'>{item.status === 1 ? ' current' : ''}</span>
-                  </div>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </>
+    <Form onSubmit={(e) => createNewLocation(e)} className='mt-2'>
+      <div className='d-flex gap-2'>
+        <Form.Group>
+          <Form.Label>Vault</Form.Label>
+          <Form.Select required onChange={(e) => updateInventory({ vault: e.target.value })} className='w-fit-content'>
+            <option value=''> - Select -</option>
+            <option value={'dallas'}>Dallas</option>
+            <option value={'delaware'}>Delaware</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Zone</Form.Label>
+          <Form.Select required onChange={(e) => updateInventory({ zone: e.target.value })} className='w-fit-content'>
+            <option value=''>- Select -</option>
+            {zoneOptions.map((item, index) => (
+              <option key={`zone-area_${index}`} value={item.toLowerCase()}>
+                {item}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        {(inventory?.zone?.toLowerCase().startsWith('credenza') ||
+          inventory?.zone?.toLowerCase().startsWith('cabinet')) && (
+          <div className='d-flex gap-2'>
+            <Form.Group>
+              <Form.Label>Shelf</Form.Label>
+              <Form.Select
+                required
+                onChange={(e) => updateInventory({ shelf: e.target.value })}
+                className='w-fit-content'
+              >
+                <option value=''>- Select -</option>
+                <option value='1'>Shelf 1</option>
+                <option value='2'>Shelf 2</option>
+                <option value='3'>Shelf 3</option>
+                <option value='4'>Shelf 4</option>
+                <option value='5'>Shelf 5</option>
+                <option value='6'>Shelf 6</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Box</Form.Label>
+              <Form.Control required onChange={(e) => updateInventory({ box: e.target.value })} className='mb-0' />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Slot</Form.Label>
+              <Form.Control required onChange={(e) => updateInventory({ slot: e.target.value })} className='mb-0' />
+            </Form.Group>
+          </div>
         )}
-        <div className='d-flex gap-2 mt-2'>
-          {initialInventory.length > 0 && (
-            <Button onClick={() => updateInventoryLocation()} disabled={isPutLoading}>
-              {isPutLoading ? (
-                <>
-                  <Spinner as='span' animation='border' role='status' aria-hidden='true' />
-                  <span className='visually-hidden'>Loading...</span>
-                </>
-              ) : (
-                'Update location'
-              )}
-            </Button>
-          )}
-          <Button
-            variant='outline-primary'
-            onClick={() => setIsAddingLocation(true)}
-            className='d-flex align-items-center gap-2'
-          >
-            <BsFillPlusCircleFill />
-            Add new location
-          </Button>
+        {inventory?.zone?.toLowerCase().endsWith('gallery wall') && (
+          <div className='d-flex gap-2'>
+            <Form.Group>
+              <Form.Label>Row</Form.Label>
+              <Form.Select
+                required
+                onChange={(e) => updateInventory({ row: e.target.value })}
+                className='w-fit-content'
+              >
+                <option value=''>- Select -</option>
+                <option value='1'>Row 1</option>
+                <option value='2'>Row 2</option>
+                <option value='3'>Row 3</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Slot</Form.Label>
+              <Form.Control required onChange={(e) => updateInventory({ slot: e.target.value })} className='mb-0' />
+            </Form.Group>
+          </div>
+        )}
+        <div className='admin-page_location-switch-wrapper'>
+          <Form.Check type='switch' id='custom-switch' label='Cascade' />
         </div>
       </div>
-      {isAddingLocation && (
-        <Form onSubmit={(e) => createNewLocation(e)} className='mt-2'>
-          <Row>
-            <Col lg={4}>
-              <Form.Group>
-                <Form.Label>Vault</Form.Label>
-                <Form.Select required onChange={(e) => updateInventory({ vault: e.target.value })}>
-                  <option value=''> - Select -</option>
-                  <option value={'dallas'}>Dallas</option>
-                  <option value={'delaware'}>Delaware</option>
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col lg={4}>
-              <Form.Group>
-                <Form.Label>Zone</Form.Label>
-                <Form.Select required onChange={(e) => updateInventory({ zone: e.target.value })}>
-                  <option value=''>- Select -</option>
-                  {zoneOptions.map((item, index) => (
-                    <option key={`zone-area_${index}`} value={item.toLowerCase()}>
-                      {item}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
-          {(inventory?.zone?.toLowerCase().startsWith('credenza') ||
-            inventory?.zone?.toLowerCase().startsWith('cabinet')) && (
-            <Row>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Shelf</Form.Label>
-                  <Form.Select required onChange={(e) => updateInventory({ shelf: e.target.value })}>
-                    <option value=''>- Select -</option>
-                    <option value='1'>Shelf 1</option>
-                    <option value='2'>Shelf 2</option>
-                    <option value='3'>Shelf 3</option>
-                    <option value='4'>Shelf 4</option>
-                    <option value='5'>Shelf 5</option>
-                    <option value='6'>Shelf 6</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Box</Form.Label>
-                  <Form.Control required onChange={(e) => updateInventory({ box: e.target.value })} />
-                </Form.Group>
-              </Col>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Slot</Form.Label>
-                  <Form.Control required onChange={(e) => updateInventory({ slot: e.target.value })} />
-                </Form.Group>
-              </Col>
-            </Row>
+      <div className='d-flex gap-2 mt-2'>
+        <Button type='submit' disabled={isPostLoading}>
+          {isPostLoading ? (
+            <>
+              <Spinner as='span' animation='border' role='status' aria-hidden='true' />
+              <span className='visually-hidden'>Loading...</span>
+            </>
+          ) : (
+            'Add location'
           )}
-          {inventory?.zone?.toLowerCase().endsWith('gallery wall') && (
-            <Row>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Row</Form.Label>
-                  <Form.Select required onChange={(e) => updateInventory({ row: e.target.value })}>
-                    <option value=''>- Select -</option>
-                    <option value='1'>Row 1</option>
-                    <option value='2'>Row 2</option>
-                    <option value='3'>Row 3</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-              <Col lg={4}>
-                <Form.Group>
-                  <Form.Label>Slot</Form.Label>
-                  <Form.Control required onChange={(e) => updateInventory({ slot: e.target.value })} />
-                </Form.Group>
-              </Col>
-            </Row>
-          )}
-          <div className='d-flex gap-2 mt-2'>
-            <Button type='submit' disabled={isPostLoading}>
-              {isPostLoading ? (
-                <>
-                  <Spinner as='span' animation='border' role='status' aria-hidden='true' />
-                  <span className='visually-hidden'>Loading...</span>
-                </>
-              ) : (
-                'Add location'
-              )}
-            </Button>
-            <Button onClick={() => setIsAddingLocation(false)} variant='outline-primary'>
-              Cancel
-            </Button>
-          </div>
-        </Form>
-      )}
-    </div>
+        </Button>
+        <Button onClick={() => setIsAddingLocation(false)} variant='outline-primary'>
+          Cancel
+        </Button>
+      </div>
+    </Form>
   );
 };
 
