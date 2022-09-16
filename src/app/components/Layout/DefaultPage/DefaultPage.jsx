0cac916f8eb94d5e8c9794503bdcce61a17bdd5f
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-16 02:01:02 +0200

    updated default page styling

--- src/app/components/Layout/DefaultPage/DefaultPage.jsx
+++ src/app/components/Layout/DefaultPage/DefaultPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './DefaultPage.scss';
+
 import UserBanner from '../../UserBanner/UserBanner';
 
 const DefaultPage = ({ hasBannerStats, children }) => {
   return (
     <div className='w-100 h-100'>
       <UserBanner />
-      <div className='w-100 h-100 bg-light text-body'>{children}</div>
+      <div className='w-100 h-100 bg-light text-body default-page_content'>{children}</div>
     </div>
   );
 };
