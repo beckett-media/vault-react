42bf24670588f3ac2bf2fae030dd574502b825c6
Louis de Tilly <louisdtb@gmail.com>
2022-09-21 17:15:38 +0200

    created empty search admin state

--- src/app/pages/Admin/AdminPage.js
+++ src/app/pages/Admin/AdminPage.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { ListGroup, Button, Form } from 'react-bootstrap';
+import { ListGroup, Button, Form, Badge } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import './AdminPage.scss';
 
@@ -23,27 +24,40 @@ const NewAdmingPage = () => {
 
             <div className='admin-page_section-table'>
               <div className='admin-page_batch-actions-wrapper'>
-                <Form.Select className='admin-page_batch-actions-select'>
+                <Form.Select disabled className='admin-page_batch-actions-select'>
                   <option value=''>Batch Actions</option>
                 </Form.Select>
-                <Button variant='outline-primary'>Apply</Button>
+                <Button disabled variant='outline-primary'>
+                  Apply
+                </Button>
+                <Badge>Coming soon</Badge>
               </div>
-              <div className='admin-page_table-wrapper'>
-                <ListGroup>
-                  <ListGroup.Item className='admin-page_table-row admin-page_table-row--header'>
-                    <Form.Check></Form.Check>
-                    <div className='text-muted text-sm'>Item Image</div>
-                    <div>Item ID</div>
-                    <div>Item Description</div>
-                    <div>Status</div>
-                    <div>Vault Location</div>
-                    <div>Action</div>
-                  </ListGroup.Item>
-                  {submissions.map((item) => (
-                    <AdminRow key={'admin_row-' + item.id} item={item} />
-                  ))}
-                </ListGroup>
-              </div>
+              {submissions.length !== 0 && (
+                <div className='admin-page_table-wrapper'>
+                  <ListGroup>
+                    <ListGroup.Item className='admin-page_table-row admin-page_table-row--header'>
+                      <Form.Check></Form.Check>
+                      <div className='text-muted text-sm'>Item Image</div>
+                      <div>Item ID</div>
+                      <div>Item Description</div>
+                      <div>Status</div>
+                      <div>Vault Location</div>
+                      <div>Action</div>
+                    </ListGroup.Item>
+                    {submissions.map((item) => (
+                      <AdminRow key={'admin_row-' + item.id} item={item} />
+                    ))}
+                  </ListGroup>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className='admin-page_content'>
+            <div className='w-100 d-flex justify-content-center align-items-center gap-3'>
+              <span>Or create a new user account</span>
+              <Link to='/admin/create-account'>
+                <Button>Create new account</Button>
+              </Link>
             </div>
           </div>
         </div>
