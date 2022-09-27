5e20fac5a6dc2818af3341f517edc1bad6ed1bfb
sDoddBeckett <SDodd@beckett.com>
2022-09-27 12:17:43 -0500

    updated

--- src/app/pages/Submission/AddressEditModal.js
+++ src/app/pages/Submission/AddressEditModal.js
@@ -5,7 +5,6 @@ import { isIncompleteAddress, mapCognitoToUser, mapUserToCognito } from '../../s
 import { states } from '../../const/states';
 import './Submission.scss';
 import { useNavigate } from 'react-router-dom';
-import { validateAddress } from '../../utils/validateAddress';
 
 const AddressEditModal = (props) => {
   const { open, onClose } = props;
@@ -42,6 +41,7 @@ const AddressEditModal = (props) => {
     } catch (err) {
       // This is an address validation error from usps api.
       setError(err.message);
+      await authContext.setAttributes(mapUserToCognito({ ...userState }));
     }
   };
 
@@ -56,14 +56,14 @@ const AddressEditModal = (props) => {
     if (!isIncompleteAddress(userState)) {
       setEditing(false);
     } else {
-      alert('Shipping Address is yet not complete');
+      alert('Address is yet not complete');
     }
   };
 
   const editModalContents = () => (
     <Form onSubmit={onEditDone}>
       <Modal.Header>
-        <Modal.Title id='contained-modal-title-vcenter'>Complete your shipping address to continue</Modal.Title>
+        <Modal.Title id='contained-modal-title-vcenter'>Complete your address to continue</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form.Label>Address</Form.Label>
