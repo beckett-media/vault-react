fb3338535c805d8d008b6b5a72698f4b08be1674
sDoddBeckett <SDodd@beckett.com>
2022-09-26 20:44:47 -0500

    updated to update images with preview

--- src/app/pages/Admin/EditImageRow.js
+++ src/app/pages/Admin/EditImageRow.js
@@ -6,7 +6,7 @@ import { TiDelete } from 'react-icons/ti';
 const EditImageRow = ({ tempState, item, error, setTempState, updateImage }) => {
   const imageUrlLast = item?.image_url?.split('/').at(-1);
   const imageRevUrlLast = item?.image_rev_url?.split('/').at(-1);
-  console.log(tempState, item);
+  console.log(tempState.image_url, item.image_url);
   return (
     <Row>
       <Col className='img_container'>
@@ -14,11 +14,22 @@ const EditImageRow = ({ tempState, item, error, setTempState, updateImage }) => 
         <Input
           placeholder='- Front -'
           value={tempState.image_url}
-          onChange={(e) => setTempState({ image_url: e.target.value })}
+          onChange={(e) => setTempState({ ...tempState, image_url: e.target.value })}
         />
-        {item.image_url && (
+        {item.image_url === tempState.image_url && (
           <>
             <img className='img_image' src={item.image_url} />
+            <div className='img_delete' onClick={() => updateImage('del-image_path')}>
+              <TiDelete className='img_delete-icon' />
+            </div>
+            <div className='img_file-name'>
+              <span>{imageUrlLast}</span>
+            </div>
+          </>
+        )}
+        {item.image_url !== tempState.image_url && tempState.image_url?.length > 0 && (
+          <>
+            <img className='img_image' src={tempState.image_url} />
             <div className='img_delete' onClick={() => updateImage('del-image_path')}>
               <TiDelete className='img_delete-icon' />
             </div>
@@ -34,11 +45,22 @@ const EditImageRow = ({ tempState, item, error, setTempState, updateImage }) => 
         <Input
           placeholder='- Reverse -'
           value={tempState.image_rev_url}
-          onChange={(e) => setTempState({ image_rev_url: e.target.value })}
+          onChange={(e) => setTempState({ ...tempState, image_rev_url: e.target.value })}
         />
-        {item.image_rev_url && (
+        {item.image_rev_url === tempState.image_rev_url && (
           <>
             <img className='img_image' src={item.image_rev_url} />
+            <div className='img_delete' onClick={() => updateImage('del-image_rev_path')}>
+              <TiDelete className='img_delete-icon' />
+            </div>
+            <div className='img_file-name'>
+              <span>{imageRevUrlLast}</span>
+            </div>
+          </>
+        )}
+        {item.image_rev_url !== tempState.image_rev_url && tempState.image_rev_url?.length > 0 && (
+          <>
+            <img className='img_image' src={tempState.image_rev_url} />
             <div className='img_delete' onClick={() => updateImage('del-image_rev_path')}>
               <TiDelete className='img_delete-icon' />
             </div>
