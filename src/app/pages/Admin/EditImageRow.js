99183be129999b1f97aa2daeeecf72514cfe229b
sDoddBeckett <SDodd@beckett.com>
2022-10-12 18:33:09 -0500

    updated to add image placeholder component

--- src/app/pages/Admin/EditImageRow.js
+++ src/app/pages/Admin/EditImageRow.js
@@ -1,54 +1,90 @@
 import { Input } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { Col, Image, Row } from 'react-bootstrap';
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { TiDelete } from 'react-icons/ti';
+import CardPlaceholder from '../../assets/CardPlaceholder';
+
+const EditImageColumn = ({ tempState, item, title, setTempState, field, updateImage }) => {
+  const imageUrlLast = item?.[field].split('/').at(-1);
+  const tempImageChanged = item[field] !== tempState[field] && tempState[field].length > 0;
+
+  return (
+    <Col className='img_container'>
+      <div>{title} Image</div>
+      <Input
+        placeholder={`- ${title} -`}
+        value={tempState[field]}
+        onChange={(e) => setTempState({ ...tempState, [field]: e.target.value })}
+      />
+      {item[field] === tempState[field] && (
+        <>
+          {item[field][0] === '.' ? (
+            <CardPlaceholder className='img_image' />
+          ) : (
+            <img className='img_image' src={item[field]} />
+          )}
+          <div
+            className='img_delete'
+            onClick={() => {
+              updateImage(`del-${field.slice(0, -4)}_path`);
+              setTempState({ ...tempState, [field]: '../../assets/beckett-card-placeholder--gray.svg' });
+            }}
+          >
+            <TiDelete className='img_delete-icon' />
+          </div>
+          <div className='img_file-name'>
+            <span>{imageUrlLast}</span>
+          </div>
+        </>
+      )}
+      {tempImageChanged && (
+        <>
+          {tempState[field][0] === '.' ? (
+            <CardPlaceholder className='img_image' />
+          ) : (
+            <img className='img_image' src={tempState[field]} />
+          )}
+          <div
+            className='img_delete'
+            onClick={() => {
+              updateImage(`del-${field.slice(0, -4)}_path`);
+              setTempState({ ...tempState, [field]: '../../assets/beckett-card-placeholder--gray.svg' });
+            }}
+          >
+            <TiDelete className='img_delete-icon' />
+          </div>
+          <div className='img_file-name'>
+            <span>{imageUrlLast}</span>
+          </div>
+        </>
+      )}
+    </Col>
+  );
+};
 
 const EditImageRow = ({ tempState, item, error, setTempState, updateImage }) => {
-  const imageUrlLast = item?.image_url?.split('/').at(-1);
-  const imageRevUrlLast = item?.image_rev_url?.split('/').at(-1);
   return (
-    <Row>
-      <Col className='img_container'>
-        <div>Front Image</div>
-        <Input
-          placeholder='- Front -'
-          value={tempState.image_url}
-          onChange={(e) => setTempState({ image_url: e.target.value })}
+    <>
+      <Row>
+        <EditImageColumn
+          tempState={tempState}
+          item={item}
+          title={'Front'}
+          field={'image_url'}
+          setTempState={setTempState}
+          updateImage={updateImage}
         />
-        {item.image_url && (
-          <>
-            <img className='img_image' src={item.image_url} />
-            <div className='img_delete'>
-              <TiDelete className='img_delete-icon' onClick={() => updateImage('del-image_url')} />
-            </div>
-            <div className='img_file-name'>
-              <span>{imageUrlLast}</span>
-            </div>
-          </>
-        )}
-        {error && <div className='error'>{error}</div>}
-      </Col>
-      <Col className='img_container'>
-        <div>Back Image</div>
-        <Input
-          placeholder='- Reverse -'
-          value={tempState.image_rev_url}
-          onChange={(e) => setTempState({ image_rev_url: e.target.value })}
+        <EditImageColumn
+          tempState={tempState}
+          item={item}
+          title={'Reverse'}
+          field={'image_rev_url'}
+          setTempState={setTempState}
+          updateImage={updateImage}
         />
-        {item.image_rev_url && (
-          <>
-            <img className='img_image' src={item.image_rev_url} />
-            <div className='img_delete'>
-              <TiDelete className='img_delete-icon' onClick={() => updateImage('del-image_rev_url')} />
-            </div>
-            <div className='img_file-name'>
-              <span>{imageRevUrlLast}</span>
-            </div>
-          </>
-        )}
-        {error && <div className='error'>{error}</div>}
-      </Col>
-    </Row>
+      </Row>
+      <Row>{error && <div className='error'>{error}</div>}</Row>
+    </>
   );
 };
 
